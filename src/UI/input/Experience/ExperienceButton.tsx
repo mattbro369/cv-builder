c5166f010c447b8de8aec86abc0f607771ccc26c
mattbro369 <ethacktry@gmail.com>
2024-04-01 21:26:53 +0200

    add experience onclick

--- src/UI/input/Experience/ExperienceButton.tsx
+++ src/UI/input/Experience/ExperienceButton.tsx
@@ -1,9 +1,17 @@
 import { Button } from '@/components/ui/button';
 
-function ButtonCustom() {
+type ButtonProps = {
+	addExperienceOnClick: () => void;
+};
+
+function ButtonCustom({ addExperienceOnClick }: ButtonProps) {
 	return (
 		<div className="flex w-full max-w-sm px-1">
-			<Button className="w-16">Add</Button>
+			<Button
+				className="w-16"
+				onClick={addExperienceOnClick}>
+				Add
+			</Button>
 		</div>
 	);
 }
